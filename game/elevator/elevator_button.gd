bb80dfdfee2f5413c6ceec5a948f9f6540203718
Tattista <tatupata@hotmail.com>
2026-01-31 13:50:31 +0200

    elevator button helper stuff yey

--- game/elevator/elevator_button.gd
+++ game/elevator/elevator_button.gd
@@ -1,9 +1,36 @@
 class_name ElavatorButton extends StaticBody3D
 
+
+var button_type: ButtonType
+enum ButtonType {
+	FLOOR_BUTTON,
+	ACTION_BUTTON,
+}
+
+var floor: int
 @onready var outline: MeshInstance3D = %Outline
+
 
 func _ready() -> void:
 	unfocus_button()
+	
+
+func set_floor(new_floor: int) -> void:
+	if button_type != ButtonType.FLOOR_BUTTON:
+		push_error("Trying to set floor on invalid button type: ", get_button_type_as_string())
+		return
+	
+	floor = new_floor
+
+
+func set_button_Type(new_button_type: ButtonType) -> void:
+	button_type = new_button_type
+
+
+func get_button_type_as_string() -> String:
+	return ButtonType.keys()[button_type]
+
+
 
 func focus_button() -> void:
 	outline.visible = true
