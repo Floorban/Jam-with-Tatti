3c9d99f65a75f011f1c927cdcffd63f8cca6b8c7
Eric Li <15696082812@163.com>
2026-01-31 15:13:18 +0100

    l

--- game/elevator/elevator_door.gd
+++ game/elevator/elevator_door.gd
@@ -8,8 +8,16 @@ class_name ElevatorDoor extends CharacterBody3D
 @export var closed_scale_x := 1.0
 @export var move_duration := 1.5
 
+var door_tween: Tween
 var is_open := true
-var tween: Tween
+var _box_shape: BoxShape3D
+var _closed_size_x: float
+var _closed_pos_x: float
+
+func _ready() -> void:
+	_box_shape = collision.shape as BoxShape3D
+	_closed_size_x = _box_shape.size.x
+	_closed_pos_x = collision.position.x
 
 func open() -> void:
 	if is_open:
@@ -21,33 +29,45 @@ func open() -> void:
 		return
 
 	is_open = true
-	door_move_tween(open_scale_x)
+	door_move_door_tween(open_scale_x)
 
 func close() -> void:
 	if not is_open:
 		return
 
 	is_open = false
-	door_move_tween(closed_scale_x)
+	door_move_door_tween(closed_scale_x)
 
-func door_move_tween(target_x: float) -> void:
-	if tween and tween.is_running():
-		tween.kill()
+func door_move_door_tween(target_ratio: float) -> void:
+	if door_tween and door_tween.is_running():
+		door_tween.kill()
 
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.set_ease(Tween.EASE_IN_OUT)
+	door_tween = create_tween()
+	door_tween.set_trans(door_tween.TRANS_SINE)
+	door_tween.set_ease(door_tween.EASE_IN_OUT)
 
-	tween.tween_property(
+	door_tween.parallel().tween_property(
 		mesh_root,
 		"scale:x",
-		target_x,
+		target_ratio,
 		move_duration
 	)
 
-	tween.tween_property(
+	var target_size_x := _closed_size_x * target_ratio
+	var target_pos_x := _closed_pos_x - (_closed_size_x - target_size_x) * 0.5
+
+	# Tween BoxShape size
+	door_tween.parallel().tween_property(
+		_box_shape,
+		"size:x",
+		target_size_x,
+		move_duration
+	)
+
+	# Tween CollisionShape position
+	door_tween.parallel().tween_property(
 		collision,
-		"scale:x",
-		target_x,
+		"position:x",
+		target_pos_x,
 		move_duration
 	)
