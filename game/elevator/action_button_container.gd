e07cc18f73bc8961c93ce9fec12fba0a3c88a25e
Tattista <tatupata@hotmail.com>
2026-01-31 13:54:32 +0200

    more button/button_container stuff

---
+++ game/elevator/action_button_container.gd
@@ -0,0 +1,9 @@
+class_name ActionButtonContainer
+extends Node3D
+
+func _ready() -> void:
+	for child in get_children():
+		if child is ElavatorButton:
+			child.button_type = child.ButtonType.ACTION_BUTTON
+		else:
+			push_error("Invalid child in ", name, ", :", child.name, "!")
--- game/elevator/floor_button_container.gd
+++ game/elevator/floor_button_container.gd
@@ -1,9 +1,10 @@
 class_name FloorButtonContainer
 extends Node3D
 
-
 func _ready() -> void:
 	for child in get_children():
 		if child is ElavatorButton:
 			child.button_type = child.ButtonType.FLOOR_BUTTON
 			child.set_floor(child.get_index())
+		else:
+			push_error("Invalid child in ", name, ", :", child.name, "!")
